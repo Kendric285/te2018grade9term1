6450152153f5c076c8447720c7cbe73a9050fad2
abdal <abdalamerx@gmail.com>
2018-11-03 15:55:24 -0400

    Haunted
    haunted

--- debugging.js
+++ debugging.js
@@ -1,38 +1,39 @@
 // Remove block comments when ready to debug a program.
 
 // Program #1: "Careless"
-/*
-let x == 3;
-if(x => 3 {
-  X+++;
-}} Else {
-  X =+ 2;
 
-console.log = X;
-*/
+let x = 3;
+if(x => 3) {
+  x++;
+} else {
+  x > 2;
+}
+
+console.log(x)
+
 
 // Program #2: "Bad logic and math"
-/*
+
 let money = 28;
-if(money > 50 && money <= 20) {
-  money /= 2;
+if(money > 20 && money <= 50) {
+  money /=2;
 } else {
-  money /= 0;
+  money >= 0;
 }
-*/
+ console.log(money);
 
 // Program #3: "And what else?"
-/*
+const READLINE = require("readline-sync");
+
 let num1 = READLINE.question("Enter num1: ");
 let num2 = READLINE.question("Enter num2: ");
 let num3 = READLINE.question("Enter num3: ");
-if(num1 > num2) {
-  console.log(`${num1} is the largest value.`);
+if(num1 < num2 && num2 > num3  ) {
+  console.log("Number2 is the greatest is the largest value.");
 }
-else if(num2 > num3) {
-  console.log(`${num2} is the largest value.`);
+else if(num1> num2 && num1 > num3) {
+  console.log("Number 1 is the largest value.");
+}else {
+  console.log("Number 3 is the largest value.");
 }
-else {
-  console.log(`${num3} is the largest value.`);
-}
-*/
+
--- haunted.js
+++ haunted.js
@@ -2,6 +2,7 @@ const READLINE = require("readline-sync");
 
 // use whatever properties you want for the player object
 let player = {
+  health: 100,
   name: undefined,
   courage: 0,
   intellect: 0,
@@ -9,23 +10,34 @@ let player = {
   charm: 0,
   weapon: "fists",
   item: "flashlight",
+  catchphrase : undefined,
 };
+
 
 console.log();
 console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
 player.name = READLINE.question("What is your name?: ");
+player.catchphrase = READLINE.question("What is Your catchphrase: ")
 console.log(`${player.name} stands in front of a spooky haunted house.`);
 console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
 let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
 if(enterHouse == "y" || enterHouse == "yes") {
   player.courage++;
-  console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
-  // continue the story
+    console.log(`${player.name} is walking in the house and finds a staircase `);
+    let staircase = READLINE.question = ("Do you decide to go up the staircase ?: ")
+      if (staircase == "y" || staircase == "yes"){
+          player.courage++;
+          player.health = - 10;
+      }else{
+
+      }
 
 } else {
   player.intellect++;
-  console.log(`${player.name} decides not to open the door. However...`);
-  // continue the story
+  console.log(`${player.name} decides not to open the door. However... a killer clown forces you back in the house`);
+   
 
 }
+
 console.log("Thanks for playing!");
+console.log("player.health");
